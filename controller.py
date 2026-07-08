ddc074d8a19187835835002ca2dcf487ad88058b
Cahree <cmyrick1@umbc.edu>
2026-07-08 13:28:42 -0400

    adding confiurations file for experiments e1-e5

--- controller.py
+++ controller.py
@@ -1,15 +1,33 @@
+import argparse
 import yaml
 
 from experiment import Experiment
 
 
-def load_config(path: str = "config.yaml"):
+def load_config(path: str):
     with open(path, "r") as f:
         return yaml.safe_load(f)
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Run a machine learning experiment."
+    )
+
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="config.yaml",
+        help="Path to the experiment configuration file.",
+    )
+
+    return parser.parse_args()
+
+
 def main():
-    cfg = load_config()
+    args = parse_args()
+
+    cfg = load_config(args.config)
     experiment = Experiment(cfg)
     experiment.run()
 
